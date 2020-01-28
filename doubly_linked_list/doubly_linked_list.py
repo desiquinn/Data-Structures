c0fbd04f4932b45174dd1eb636dd7655fa3b7f84
Desiree Morris <desiquinncompany@gmail.com>
2020-01-28 17:06:55 -0500

    got all doubly-linked tests passing

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -96,36 +96,48 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
         self.length -= 1
         # if empty
-        if not self.head and not self.tail
+        if not self.head and not self.tail:
+            return None
         #if head and tail
-        if self.head and self.tail:
+        if self.head == self.tail:
             self.head = None
             self.tail = None
         #if head
-        elif self.head:
-            self.next.prev = None
-            self.head = self.next
+        elif self.head == node:
+            self.head = self.head.next
+            node.delete()
         #if tail
-        elif self.tail:
-            self.prev.next = None
-            self.tail = self.prev
+        elif self.tail == node:
+            self.tail = self.tail.prev
+            node.delete()
         #all others
         else:
-            self.next.prev = self.prev
-            self.prev.next = self.next
-        
+            node.delete()
     """Returns the highest value currently in the list"""
     def get_max(self):
-        pass
+        #if empty
+        if self.head is None:
+            return None
+        max_value = self.head.value
+        current = self.head
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
