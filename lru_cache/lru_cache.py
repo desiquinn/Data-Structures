516e087a3fc66e3bd468695f47b28d964d3cbd12
Desiree Morris <desiquinncompany@gmail.com>
2020-01-30 18:00:34 -0500

    completed lru_cache with all tests passing.  Had to fix one of the tests

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+from doubly_linked_list import DoublyLinkedList
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +9,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.capacity = limit
+        self.current_size = 0
+        self.dll = DoublyLinkedList()
+        self.storage = {}
 
     """
     Retrieves the value associated with the given key. Also
@@ -17,7 +22,23 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        # if key value pair doesn't exist in self.storage
+        if key not in self.storage:
+            # return None
+            print(self.storage)
+            return None
+        # if key value pair does exist in self.storage
+        else:
+            # retrieves value that belongs to key
+            print(self.storage)
+            node = self.storage[key]
+            print(node)
+            # value = node.value
+            # move key-value pair to the end of the order
+            self.dll.remove_from_tail()
+            self.dll.add_to_head(node)
+            # returns the value associated with the key
+            return node
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +51,23 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        # if key already exists in the cache
+        if key in self.storage:
+            # overwrite the old value associated with the key with the new value
+            self.storage[key] = value       
+        # if the cache is at max capacity
+        if self.current_size == self.capacity:
+            # remove oldest entry in cache (tail)
+            self.dll.remove_from_tail()
+            # add given key value pair to cache to the head
+            self.dll.add_to_head((key,value))
+            self.storage[key] = value
+        # else if cache not at max capacity and key doesn't exsist
+        else:
+            # add given key value pair to cache to the head
+            self.dll.add_to_head((key,value))
+            self.storage[key] = value
+            # increment current size
+            self.current_size += 1
+
+
